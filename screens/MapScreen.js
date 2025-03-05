e4621b05a4949a83994ed0fffb8ce1904ae9840d
Arno <arnobaret@yahoo.fr>
2025-03-05 20:32:40 +0100

    Pointeur utilisateur et recentrage

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,41 +1,93 @@
-import React, { useEffect, useState } from 'react';
-import { View, StyleSheet, Dimensions } from 'react-native';
+import React, { useEffect, useState, useRef } from 'react';
+import { View, StyleSheet, Dimensions, Image } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 import * as Location from 'expo-location';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function HomeScreen({ navigation }) {
-    const [location, setLocation] = useState(null);
 
+    //images pointeurs
+    const markers = {
+        pinUser: require("../assets/pinUser.png"),
+		pinGame: require("../assets/pinGame.png"),
+		pinGameOff: require("../assets/pinGameOff.png")
+    }
+
+    // Référence à la carte
+    const mapRef = useRef(null);
+
+    //etat pour de la position de l'utilisateur
+    const [userLocation, setUserLocation] = useState({
+        latitude: 0,
+        longitude: 0,
+    });
+
+    // localisation de l'utilisateur
     useEffect(() => {
         (async () => {
+            console.log('here');
             const { status } = await Location.requestForegroundPermissionsAsync();
+            console.log(status);
+            // demande de permission pour la Geolocalisation
             if (status === 'granted') {
-                Location.watchPositionAsync(
-                    { distanceInterval: 10 },
+                console.log('here 2');
+                // si autorisation alors on recupere la position
+                
+                Location.watchPositionAsync({ distanceInterval: 10 },
                     (loc) => {
-                        setLocation(loc.coords);
-                        console.log(loc);
-                    }
-                );
+                        console.log('here 3')
+                        setUserLocation(loc.coords);
+                        console.log(loc.coords);
+                    });
             }
         })();
     }, []);
 
+    // Fonction pour vérifier si l'utilisateur est hors du cadre
+    const onRegionChangeComplete = (newRegion) => {
+        const { latitude, longitude } = newRegion;
+        const { latitude: userLatitude, longitude: userLongitude } = userLocation;
+        const LATITUDE_DELTA = 0.0922;
+        const LONGITUDE_DELTA = 0.0421;
+
+        if (
+            latitude < userLatitude - LATITUDE_DELTA / 2 ||
+            latitude > userLatitude + LATITUDE_DELTA / 2 ||
+            longitude < userLongitude - LONGITUDE_DELTA / 2 ||
+            longitude > userLongitude + LONGITUDE_DELTA / 2
+        ) {
+            mapRef.current.animateToRegion({
+                latitude: userLatitude,
+                longitude: userLongitude,
+                latitudeDelta: LATITUDE_DELTA,
+                longitudeDelta: LONGITUDE_DELTA,
+            }, 1000);
+        }
+    };
+
     return (
         <View style={styles.container}>
-            {/* <SafeAreaView /> */}
             <MapView
+                ref={mapRef}
+                mapType="terrain"
                 style={styles.map}
                 initialRegion={{
-                    latitude: location?.latitude || 48.866667,
-                    longitude: location?.longitude || 2.333333,
+                    latitude: userLocation.latitude || 48.866667,
+                    longitude: userLocation.longitude || 2.333333,
                     latitudeDelta: 0.0922,
                     longitudeDelta: 0.0421,
                 }}
+                onRegionChangeComplete={onRegionChangeComplete}
             >
-                {location && (
-                    <Marker coordinate={{ latitude: location.latitude, longitude: location.longitude }} />
+                {userLocation && (
+                    <Marker
+                        coordinate={userLocation}
+                        title="My position"
+                        pinColor="red"
+                        style={styles.userPin}
+                    >
+                        <Image source={markers.pinUser} style={styles.userPin} />
+                    </Marker>
                 )}
             </MapView>
         </View>
@@ -46,8 +98,14 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
     },
+
     map: {
         width: Dimensions.get('window').width,
         height: Dimensions.get('window').height,
+    },
+
+    userPin: {
+        width: 100,
+        height: 100,
     },
 });
