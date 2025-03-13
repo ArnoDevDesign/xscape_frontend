fbcd2419563414094ca9ffd18404d4436937295e
jm.yakan@gmail.com <jm.yakan@gmail.com>
2025-03-13 09:44:37 +0100

    style screen Ingame3 et End + ajout asset animation rocket et confetti

--- screens/EndScreen.js
+++ screens/EndScreen.js
@@ -1,11 +1,13 @@
-import React, { use, useEffect, useState } from 'react';
-import { View, StyleSheet, Text, Button, TouchableOpacity, Image, Modal, TextInput, FlatList, ScrollView } from 'react-native';
+import React, { useRef, useEffect, useState } from 'react';
+import { View, StyleSheet, Text, TouchableOpacity, Image, FlatList, ScrollView, ImageBackground, Animated, Easing, Dimensions } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useDispatch, useSelector } from 'react-redux';
 import { addUserToStore, userLogout } from '../reducers/users'
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import LottieView from "lottie-react-native";
 
 const URL = process.env.EXPO_PUBLIC_BACKEND_URL
+
 export default function EndScreen({ navigation }) {
     const dispatch = useDispatch();
     const userRedux = useSelector((state) => state.users.value)
@@ -14,13 +16,15 @@ export default function EndScreen({ navigation }) {
     const [conclusionScenario, setConclusionScenario] = useState('')
     const [conclucionscenariofailed, setconclucionscenariofailed] = useState('')
     const [score, setScore] = useState(0)
+    const confettiRef = useRef(null);
+    const rocketRef = useRef(null);
 
-
+    //Récupération des données du scénario
     useEffect(() => {
         fetch(`${URL}/scenarios/${userRedux.scenario}`)
             .then(response => response.json())
             .then(data => {
-                console.log(data);
+                // console.log(data);
                 setConclusionScenario(data.conclusionScenario)
                 setconclucionscenariofailed(data.conclusionScenarioFailed)
                     // setValidated(data.sessions)
@@ -30,82 +34,191 @@ export default function EndScreen({ navigation }) {
             });
     }, [])
 
+    //ANIMATION LOTTIEFILES
+    useEffect(() => {
+        if (validated) {
+            confettiRef.current?.play();
+            rocketRef.current?.play();
+        }
+    }, [validated]);
+
+
+    // ANIMATION TEXT
+    const [textHeight, setTextHeight] = useState(0); // Stocke la hauteur du texte
+    const boxHeight = 100; // Hauteur fixe de la boîte contenant le texte
+    const lineHeight = 50; // Hauteur approximative d'une ligne de texte
+    const translateY = useRef(new Animated.Value(0)).current; // On démarre à 0
+
+    const duration = 35000; // Durée du défilement
+
+    useEffect(() => {
+        if (textHeight > 0) {
+            translateY.setValue(boxHeight + lineHeight); // Position initiale : texte en bas de la boîte
+            Animated.loop(
+                Animated.timing(translateY, {
+                    toValue: -textHeight, // Défilement vers le haut
+                    duration: duration,
+                    easing: Easing.linear,
+                    useNativeDriver: true,
+                })
+            ).start();
+        }
+    }, [textHeight]); // Attendre d'avoir la hauteur du texte
+
+    // -- JSX -- //
     return (
         <View style={styles.container}>
             <SafeAreaView />
+            <ImageBackground source={require('../assets/imgsAventure/FondAventure01X.png')}
+                style={{ width: '100%', height: '100%', position: 'absolute' }} />
+            <LottieView ref={confettiRef} source={require('../assets/Animation_confetti.json')}
+                style={styles.confetti}
+                loop={false}
+                speed={0.75}
+            />
+            <LottieView ref={rocketRef} source={require('../assets/Animation_rocket.json')}
+                style={styles.rocket}
+                loop={false}
+                speed={0.6}
+            />
             <View style={styles.titleBox}>
-                {validated ? <Text style={styles.textTitle}>BRAVO ! Mission Accomplie {username}, et avec BRIO !!!</Text> : <Text style={styles.textTitle}>Dommage {username}, vous n'avez pas réussi à nous sauver à temps...</Text>}
+                {validated ? <Text style={styles.textTitle}>BRAVO !!!</Text>
+                    : <Text style={styles.textTitle}>Dommage {username}, vous n'avez pas réussi à nous sauver à temps...</Text>}
+                {validated ? <Text style={styles.textTitle}>Mission Accomplie {username}, et avec BRIO !!!</Text>
+                    : <Text style={styles.textTitle}>Dommage {username}, vous n'avez pas réussi à nous sauver à temps...</Text>}
             </View>
-            {validated ? <ScrollView style={styles.conclusionBox}>
-                <Text style={styles.textConclusion}>{conclusionScenario}</Text>
-            </ScrollView> : <ScrollView style={styles.conclusionBox}>
-                <Text style={styles.textConclusion}>{conclucionscenariofailed}</Text>
-            </ScrollView>}
+
+            <View style={styles.conclusionBox}>
+                <Animated.View style={{ transform: [{ translateY }] }}>
+                    <Text
+                        onLayout={(event) => setTextHeight(event.nativeEvent.layout.height)}
+                        style={styles.textConclusion}
+                    >
+                        {validated ? conclusionScenario : conclucionscenariofailed}
+                    </Text>
+                </Animated.View>
+            </View>
+
             <View style={styles.buttonContainer}>
-                {validated && <Text style={styles.points}>Vous avez gagné {userRedux.scoreSession} points !</Text>}
-                <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Map')}>
-                    <Text style={styles.textButton}>Retour à l'accueil</Text>
+                <View style={styles.pointsBox}>
+                    {validated && <Text style={styles.textPoints}>Vous avez gagné : </Text>}
+                    {validated && <Text style={styles.textPoints}> {userRedux.scoreSession} points !</Text>}
+                </View>
+
+                <TouchableOpacity onPress={() => navigation.navigate('Home')}>
+                    <View style={styles.button}>
+                        <Image source={require('../assets/imgsAventure/bbtnOffX.png')} style={styles.ImageButton} />
+                        <Text style={styles.textButton}>Retour à l'accueil</Text>
+                    </View>
                 </TouchableOpacity>
             </View>
         </View>
     )
 }
 
+// -- STYLES -- //
 const styles = StyleSheet.create({
-
     container: {
         flex: 1,
         width: '100%',
-        backgroundColor: 'green',
+        // backgroundColor: 'green',
         justifyContent: "center",
         alignItems: 'center'
     },
+    //Title Box
     titleBox: {
         flex: 1,
         width: '80%',
-        backgroundColor: 'green',
-        justifyContent: "center",
-        alignItems: 'center'
-    },
-    conclusionBox: {
-        flex: 1,
-        width: '80%',
-        backgroundColor: 'green',
-        // justifyContent: "center",
-        // alignItems: 'center'
-    },
-    buttonContainer: {
-        flex: 1,
-        width: '100%',
-        backgroundColor: 'green',
+        // backgroundColor: 'green',
+        // opacity: 0.4,
         justifyContent: "center",
         alignItems: 'center'
     },
     textTitle: {
-        fontSize: 40,
+        fontFamily: 'Goldman-Regular.ttf',
+        fontSize: 35,
         color: 'white',
         textAlign: 'center'
+    },
+    //Conclusion Box
+    conclusionBox: {
+        flex: 1,
+        width: '80%',
+        // backgroundColor: 'blue',
+        // opacity: 0.4,
+        // paddingTop: 150,
+        height: 100, // Taille fixe
+        overflow: 'hidden', // Masque le texte dépassant
+        justifyContent: 'center',
     },
     textConclusion: {
-        fontSize: 20,
+        fontFamily: 'Goldman-Regular.ttf',
+        textAlign: 'justify',
+        fontSize: 24,
         color: 'white',
-        textAlign: 'center'
+        textAlign: 'center',
+        position: 'absolute', // Permet d'appliquer `translateY`
+
     },
-    textStars: {
-        fontSize: 20,
+    //Points Box
+    pointsBox: {
+        flex: 1,
+        width: '80%',
+        // backgroundColor: 'yellow',
+        // opacity: 0.4,
+        justifyContent: "center",
+        alignItems: 'center'
+    },
+    textPoints: {
+        fontFamily: 'Goldman-Regular.ttf',
+        fontSize: 30,
         color: 'white',
-        textAlign: 'center'
+        textAlign: 'center',
+    },
+    //Button Container
+    buttonContainer: {
+        flex: 1,
+        width: '80%',
+        // backgroundColor: 'red',
+        // opacity: 0.4,
+        justifyContent: "center",
+        alignItems: 'center'
     },
     button: {
-        backgroundColor: 'white',
-        padding: 10,
-        borderRadius: 10,
-        height: 50,
-        width: '80%',
+        alignItems: 'center',
+        height: 100,
+        width: '50%',
+    },
+    ImageButton: {
+        width: '100%',
+        height: '100%',
+        resizeMode: 'contain', // Ajuste l'image sans déformation
+        position: 'absolute', // Place l'image en arrière-plan
     },
     textButton: {
-        fontSize: 20,
-        color: 'green',
-        textAlign: 'center'
-    }
+        fontFamily: 'PressStart2P-Regular.ttf',
+        fontSize: 16,
+        color: 'white',
+        textAlign: 'center',
+        position: 'absolute', // Place le texte au-dessus de l'image
+        width: '100%',
+        top: '50%',
+        transform: [{ translateY: -15 }] // Ajuste la position verticale
+    },
+    // Animation Confetti
+    confetti: {
+        position: 'absolute',
+        // backgroundColor: 'blue',
+        // opacity: 0.2,
+        width: '100%',
+        height: '100%',
+    },
+    rocket: {
+        position: 'absolute',
+        top: 50,
+        // backgroundColor: 'blue',
+        // opacity: 0.2,
+        width: 780,
+        height: 780,
+    },
 })
--- screens/IngameScreen3.js
+++ screens/IngameScreen3.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useRef } from "react";
-import { StyleSheet, View, Text, TouchableOpacity, Modal, Animated } from "react-native";
+import { StyleSheet, View, Text, TouchableOpacity, Modal, Animated, ImageBackground, Image } from "react-native";
 import { addUserToStore } from "../reducers/users";
 import { useDispatch, useSelector } from "react-redux";
 import { useIsFocused } from "@react-navigation/native";
@@ -32,9 +32,6 @@ export default function Ingame3Screen({ navigation }) {
         if (gameOver) return; // Empêcher les clics après un échec
 
         const newOrder = [...pressedOrder, buttonNumber];
-
-
-
 
 
         if (newOrder.every((num, index) => num === correctSequence[index])) {
@@ -109,7 +106,6 @@ export default function Ingame3Screen({ navigation }) {
                 },
                 body: JSON.stringify({ score: score, result: game3 }),
             });
-
             const data = await response.json();
             console.log("Score mis à jour dans la base de données", data);
 
@@ -126,35 +122,52 @@ export default function Ingame3Screen({ navigation }) {
         }
     }
 
-
+    // -- JSX --//
     return (
         <View style={styles.container}>
-            <Animated.View style={[styles.titleContainer, gameOver ? styles.gameOver : shakeAnim.interpolate({
-                inputRange: [0, 1],
-                outputRange: ["#37474F", "#B71C1C"]
-            })]}>
-                <Text style={styles.title}>{title}</Text>
-                {/* <Text style={styles.score}>Score: {score}</Text> */}
-            </Animated.View>
+            <ImageBackground source={require('../assets/imgsAventure/FondAventure01X.png')} style={styles.ImageBackground} />
+            <View>
+                <Animated.View style={[styles.titleContainer, gameOver ? styles.gameOver : shakeAnim.interpolate({
+                    inputRange: [0, 1],
+                    outputRange: ["#37474F", "#B71C1C"]
+                })]}>
+                    <Image source={require('../assets/imgsAventure/modaleSimpleX.png')} />
+                    <Text style={styles.title}>{title}</Text>
+                    {/* <Text style={styles.score}>Score: {score}</Text> */}
+                </Animated.View>
+            </View>
+
 
             <View style={styles.buttonContainer}>
                 <TouchableOpacity disabled={gameOver} onPress={() => handlePress(1)} style={styles.metalButton}>
-                    <Text style={styles.metalButtonText}>{bouton1}</Text>
+                    <View style={styles.button}>
+                        <Image source={require('../assets/imgsAventure/bbtnOffX.png')} style={styles.ImageButton} />
+                        <Text style={styles.textButton}>{bouton1}</Text>
+                    </View>
                 </TouchableOpacity>
             </View>
             <View style={styles.buttonContainer}>
-                <TouchableOpacity disabled={gameOver} onPress={() => handlePress(2)} style={styles.metalButton}>
-                    <Text style={styles.metalButtonText}>{bouton2}</Text>
+                <TouchableOpacity disabled={gameOver} onPress={() => handlePress(1)} style={styles.metalButton}>
+                    <View style={styles.button}>
+                        <Image source={require('../assets/imgsAventure/bbtnOffX.png')} style={styles.ImageButton} />
+                        <Text style={styles.textButton}>{bouton2}</Text>
+                    </View>
                 </TouchableOpacity>
             </View>
             <View style={styles.buttonContainer}>
-                <TouchableOpacity disabled={gameOver} onPress={() => handlePress(3)} style={styles.metalButton}>
-                    <Text style={styles.metalButtonText}>{bouton3}</Text>
+                <TouchableOpacity disabled={gameOver} onPress={() => handlePress(1)} style={styles.metalButton}>
+                    <View style={styles.button}>
+                        <Image source={require('../assets/imgsAventure/bbtnOffX.png')} style={styles.ImageButton} />
+                        <Text style={styles.textButton}>{bouton3}</Text>
+                    </View>
                 </TouchableOpacity>
             </View>
             <View style={styles.buttonContainer}>
-                <TouchableOpacity disabled={gameOver} onPress={() => handlePress(4)} style={styles.metalButton}>
-                    <Text style={styles.metalButtonText}>{bouton4}</Text>
+                <TouchableOpacity disabled={gameOver} onPress={() => handlePress(1)} style={styles.metalButton}>
+                    <View style={styles.button}>
+                        <Image source={require('../assets/imgsAventure/bbtnOffX.png')} style={styles.ImageButton} />
+                        <Text style={styles.textButton}>{bouton4}</Text>
+                    </View>
                 </TouchableOpacity>
             </View>
 
@@ -164,6 +177,7 @@ export default function Ingame3Screen({ navigation }) {
                     <View style={styles.modalContent}>
                         <Text style={styles.modalText}>{indice1}</Text>
                         <TouchableOpacity onPress={resetGame} style={styles.modalButton}>
+                            <Image source={require('../assets/imgsAventure/bbtnOffX.png')} />
                             <Text style={styles.modalButtonText}>Réessayer</Text>
                         </TouchableOpacity>
                     </View>
@@ -172,6 +186,7 @@ export default function Ingame3Screen({ navigation }) {
             {game3 && (<Modal visible={game3} animationType="slide" transparent>
                 <View style={styles.modalContainer}>
                     <View style={styles.modalContent}>
+                        <Image source={require('../assets/imgsAventure/modaleSimpleX.png')} />
                         <Text style={styles.modalText}>PAS MAL CAAAAAAA LAAAAAAAAA </Text>
                         <TouchableOpacity onPress={finishGame} style={styles.modalButton}>
                             <Text style={styles.modalButtonText}>FINI !!!!!!!!!!</Text>
@@ -183,64 +198,96 @@ export default function Ingame3Screen({ navigation }) {
     );
 }
 
+// -- STYLES --//
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: "black",
+        // backgroundColor: "black",
         alignItems: "center",
         justifyContent: "center",
         width: "100%",
         height: "100%",
         padding: 10,
     },
+    //TITRE
     titleContainer: {
         width: "100%",
-        height: "15%",
+        height: "25%",
         alignItems: "center",
         justifyContent: "center",
-        backgroundColor: "#37474F",
-        borderRadius: 10,
-        marginBottom: 20,
+        position: "relative",  // Définit une position relative pour les enfants
+    },
+    title: {
+        fontSize: 25,
+        fontFamily: 'Goldman-Regular.ttf',
+        padding: 50,
+        color: "white",
+        position: "absolute",  // Superpose le texte à l’image
+        top: "30%",            // Centre verticalement
+        left: "10%",           // Centre horizontalement
+        transform: [{ translateX: -50 }, { translateY: -50 }], // Ajuste le centrage
+        textAlign: "center",
+        width: "100%",
     },
     gameOver: {
         backgroundColor: "#B71C1C",
-    },
-    title: {
-        fontSize: 18,
-        color: "white",
-        fontWeight: "bold",
     },
     score: {
         fontSize: 16,
         color: "yellow",
         marginTop: 5,
     },
+    //BOUTON
     buttonContainer: {
         width: "100%",
         height: "15%",
         alignItems: "center",
         justifyContent: "center",
+        flexDirection: "column",
         marginVertical: 5,
-    },
-    metalButton: {
-        width: "70%",
-        height: "100%",
-        backgroundColor: "#607D8B",
-        alignItems: "center",
-        justifyContent: "center",
-        borderRadius: 10,
-        borderWidth: 2,
-        borderColor: "#B0BEC5",
         shadowColor: "#000",
         shadowOffset: { width: 2, height: 2 },
         shadowOpacity: 0.8,
         shadowRadius: 5,
     },
-    metalButtonText: {
-        fontSize: 16,
-        fontWeight: "bold",
-        color: "white",
+    button: {
+        position: "relative",  // Permet aux enfants d'être positionnés par rapport à ce conteneur
+        alignItems: "center",
+        justifyContent: "center",
+        width: "100%",
+        height: "100%",
     },
+    textButton: {
+        fontFamily: 'Goldman-Bold.ttf',
+        fontSize: 30,
+        color: "green",
+        position: "absolute",  // Superpose le texte à l’image
+        top: "80%",            // Place au milieu verticalement
+        left: "15%",           // Place au milieu horizontalement
+        transform: [{ translateX: -50 }, { translateY: -50 }], // Centre parfaitement
+        textAlign: "center",
+        width: "100%",
+    },
+
+    // metalButton: {
+    //     width: "70%",
+    //     height: "100%",
+    //     backgroundColor: "#607D8B",
+    //     alignItems: "center",
+    //     justifyContent: "center",
+    //     borderRadius: 10,
+    //     borderWidth: 2,
+    //     borderColor: "#B0BEC5",
+    //     shadowColor: "#000",
+    //     shadowOffset: { width: 2, height: 2 },
+    //     shadowOpacity: 0.8,
+    //     shadowRadius: 5,
+    // },
+    // metalButtonText: {
+    //     fontSize: 16,
+    //     fontWeight: "bold",
+    //     color: "white",
+    // },
     modalContainer: {
         flex: 1,
         backgroundColor: "rgba(0, 0, 0, 0.8)",
@@ -249,7 +296,7 @@ const styles = StyleSheet.create({
     },
     modalContent: {
         width: "80%",
-        backgroundColor: "#37474F",
+        // backgroundColor: "#37474F",
         padding: 20,
         borderRadius: 10,
         alignItems: "center",
@@ -258,17 +305,24 @@ const styles = StyleSheet.create({
         fontSize: 18,
         color: "white",
         marginBottom: 15,
-        textAlign: "center",
+        textAlign: "center", // Centre le texte dans la modal
     },
     modalButton: {
-        backgroundColor: "#FF6F00",
-        paddingVertical: 10,
-        paddingHorizontal: 20,
-        borderRadius: 5,
+        position: "relative",  // Permet aux enfants d'être positionnés par rapport à ce conteneur
+        alignItems: "center",
+        justifyContent: "center",
+        width: "100%",
+        height: "100%",
     },
     modalButtonText: {
-        fontSize: 16,
-        fontWeight: "bold",
+        fontFamily: 'Goldman-Bold.ttf',
+        fontSize: 45,
         color: "white",
+        position: "absolute",  // Superpose le texte à l’image
+        top: "52%",            // Place au milieu verticalement
+        left: "20%",           // Place au milieu horizontalement
+        transform: [{ translateX: -50 }, { translateY: -50 }], // Centre parfaitement
+        textAlign: "center",
+        width: "100%",
     },
 });
