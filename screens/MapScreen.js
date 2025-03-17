c5dddb5b44c4446faed0fac0e0a7f192d95202db
fabio fernandes <fabio.f@outlook.fr>
2025-03-17 16:19:10 +0100

    commit

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -47,9 +47,9 @@ const getDistanceFromLatLonInMeters = (lat1, lon1, lat2, lon2) => {
   const a =
     Math.sin(dLat / 2) * Math.sin(dLat / 2) +
     Math.cos((lat1 * Math.PI) / 180) *
-      Math.cos((lat2 * Math.PI) / 180) *
-      Math.sin(dLon / 2) *
-      Math.sin(dLon / 2);
+    Math.cos((lat2 * Math.PI) / 180) *
+    Math.sin(dLon / 2) *
+    Math.sin(dLon / 2);
   const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
   return R * c;
 };
@@ -229,7 +229,7 @@ export default function MapScreen({ navigation }) {
   if (!loaded) {
     return null;
   }
-  
+
   return isLoading || geolocationError ? (
     <SafeAreaView style={{ flex: 1 }}>
       <View style={styles.loaderContainer}>
